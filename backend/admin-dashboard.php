bfbdd07415a8a7abee846b599749e497eb6dc8a4
Nasir <nasirsabbir07@gmail.com>
2024-11-26 12:17:06 +0600

    link duration and course price

--- backend/admin-dashboard.php
+++ backend/admin-dashboard.php
@@ -23,41 +23,90 @@ function dubkii_booking_admin_page(){
     $durations_table = $wpdb->prefix . "dubkii_course_durations";
     $start_dates_table = $wpdb->prefix . "dubkii_course_start_dates";
     $personal_details_table = $wpdb->prefix . 'dubkii_personal_details';
+    $course_prices_table = $wpdb->prefix . 'dubkii_courses_prices';
 
     //Handle form submission for adding new courses
     if(isset($_POST['submit_course'])){
         $course_name = sanitize_text_field($_POST['course_name']);
-        $price = floatval($_POST['price']);
-
-        $wpdb->insert($courses_table, array(
-            'course_name' => $course_name,
-            'price' => $price
-        ));
-        $course_id = $wpdb->insert_id;
-
-        // Insert multiple durations
         $durations = $_POST['durations'];
-        foreach ($durations as $duration_weeks) {
-            if (!empty($duration_weeks)) {
-                $wpdb->insert($durations_table, array(
-                    'course_id' => $course_id,
-                    'duration_weeks' => intval($duration_weeks)
-                ));
-            }
-        }
-
-        // Insert multiple start dates
         $start_dates = $_POST['start_dates'];
-        foreach ($start_dates as $start_date) {
-            if (!empty($start_date)) {
-                $wpdb->insert($start_dates_table, array(
-                    'course_id' => $course_id,
-                    'start_date' => sanitize_text_field($start_date)
-                ));
-            }
-        }
+        $prices = array_map('floatval',($_POST['prices']));
 
+        try{  
+            // start transaction
+            $wpdb->query('START TRANSACTION');
+            
+            // Insrt into courses table
+            $wpdb->insert($courses_table, array(
+            'course_name' => $course_name,
+            ));
+            $course_id = $wpdb->insert_id;
+
+            if (!$course_id) {
+                throw new Exception('Failed to insert into courses table.');
+            }
+
+            // Insert multiple durations and their prices
+            foreach ($durations as $index=>$duration_weeks) {
+                if (!empty($duration_weeks)) {
+                    $wpdb->insert($durations_table, array(
+                        'course_id' => $course_id,
+                        'duration_weeks' => intval($duration_weeks)
+                    ));
+                    $duration_id = $wpdb->insert_id;
+
+                    if(!$duration_id){
+                        throw new Exception('Failed to insert into durations table.');
+                    }
+                    error_log("Inserted duration with ID: $duration_id");
+                    // Insert into prices table for each duration
+                    if(isset($prices[$index]) && !empty($prices[$index])){
+                        $wpdb->insert($course_prices_table, array(
+                            'course_id' => $course_id,
+                            'duration_id' => $duration_id,
+                            'price' => floatval($prices[$index])
+                        ));
+                        $price_id = $wpdb->insert_id;
+                        if(!$price_id){
+                            throw new Exception('Failed to insert into prices table.');
+                        }
+                        error_log("Inserted price: {$prices[$index]} for duration ID: $duration_id");
+                    }else {
+                        error_log("Price not set or empty for duration index $index");
+                    }
+                }
+            }
+
+            // Insert multiple start dates
+            
+            foreach ($start_dates as $start_date) {
+                if (!empty($start_date)) {
+                    $wpdb->insert($start_dates_table, array(
+                        'course_id' => $course_id,
+                        'start_date' => sanitize_text_field($start_date)
+                    ));
+
+                    if(!$wpdb->insert_id){
+                        throw new Exception('Failed to insert into start dates table.');
+                    }
+                }
+            }
+            // Commit the transaction
+            $wpdb->query('COMMIT');
+            error_log("Transaction committed successfully.");
+            add_settings_error('dubkii_booking', 'course_add_success', 'Course added successfully!', 'updated');
+            // wp_redirect(add_query_arg('active_tab', 'courses', $_SERVER['REQUEST_URI']));
+            // exit;
+        }catch (Exception $e) {
+            // Rollback transaction in case of any error
+            $wpdb->query('ROLLBACK');
+            add_settings_error('dubkii_booking', 'course_add_error', 'An error occurred: ' . $e->getMessage(), 'error');
+        }
+            
     }
+
+    // Display error or success messages
+    settings_errors('dubkii_booking');
 
 
     // Pagination variables
@@ -67,11 +116,13 @@ function dubkii_booking_admin_page(){
 
     // Fetch all courses with durations and start dates
     $courses = $wpdb->get_results("
-        SELECT c.id, c.course_name, c.price, 
+        SELECT c.id, c.course_name, 
         GROUP_CONCAT(DISTINCT d.duration_weeks ORDER BY d.duration_weeks) AS durations,
+        GROUP_CONCAT(DISTINCT cp.price ORDER BY d.duration_weeks) AS prices,
         GROUP_CONCAT(DISTINCT s.start_date ORDER BY s.start_date) AS start_dates
         FROM $courses_table c
         LEFT JOIN $durations_table d ON c.id = d.course_id
+        LEFT JOIN $course_prices_table cp ON c.id = cp.course_id AND d.id = cp.duration_id
         LEFT JOIN $start_dates_table s ON c.id = s.course_id
         GROUP BY c.id
         LIMIT $courses_per_page OFFSET $offset
@@ -82,6 +133,7 @@ function dubkii_booking_admin_page(){
         SELECT COUNT(DISTINCT c.id)
         FROM $courses_table c
         LEFT JOIN $durations_table d ON c.id = d.course_id
+        LEFT JOIN $course_prices_table cp ON c.id = cp.course_id AND d.id = cp.duration_id
         LEFT JOIN $start_dates_table s ON c.id = s.course_id
     ");
 
@@ -95,9 +147,12 @@ function dubkii_booking_admin_page(){
 
     // Fetch all booked courses from personal details table
     $booked_courses = $wpdb->get_results("
-        SELECT pd.id, pd.name, pd.course_id, c.course_name, pd.start_date, pd.duration
+        SELECT pd.id, pd.name, pd.course_id, c.course_name, pd.start_date, pd.duration, pd.total_amount, pd.transport_cost
         FROM $personal_details_table pd
         LEFT JOIN $courses_table c ON pd.course_id = c.id
+        LEFT JOIN $durations_table d ON pd.course_id = d.course_id AND pd.duration = d.duration_weeks
+        LEFT JOIN $course_prices_table cp ON c.id = cp.course_id AND d.id = cp.duration_id
+        ORDER BY pd.id DESC
         LIMIT $booked_courses_per_page OFFSET $booked_courses_offset
     ");
 
@@ -107,6 +162,8 @@ function dubkii_booking_admin_page(){
         SELECT COUNT(*)
         FROM $personal_details_table pd
         LEFT JOIN $courses_table c ON pd.course_id = c.id
+        LEFT JOIN $durations_table d ON pd.course_id = d.course_id AND pd.duration = d.duration_weeks
+        LEFT JOIN $course_prices_table cp ON c.id = cp.course_id AND d.id = cp.duration_id
     ");
 
     // Calculate total pages for booked courses
@@ -135,14 +192,14 @@ function dubkii_booking_admin_page(){
                         <td><input type="text" name="course_name" id="course_name" required></td>
                     </tr>
                     <tr>
-                        <th><label for="price">Price</label></th>
-                        <td><input type="number" step="0.01" name="price" id="price" required></td>
-                    </tr>
-                    <tr>
                         <th><label for="durations">Durations (weeks)</label></th>
                         <td>
                             <div id="duration-wrapper">
-                                <input type="number" name="durations[]" placeholder="Duration in weeks">
+                                <div class="duration-field">
+                                    <input type="number" name="durations[]" placeholder="Duration in weeks" required>
+                                    <input type="number" name="prices[]" step="0.01" placeholder="Price" required>
+                                </div>
+                                
                             </div>
                             <button type="button" onclick="addDurationField()">Add Another Duration</button>
                         </td>
@@ -176,9 +233,9 @@ function dubkii_booking_admin_page(){
                         <tr>
                             <td><?php echo $course->id; ?></td>
                             <td><?php echo $course->course_name; ?></td>
-                            <td><?php echo $course->start_dates; ?></td>
-                            <td><?php echo $course->durations; ?></td>
-                            <td><?php echo $course->price; ?></td>
+                            <td><?php echo implode(', ', explode(',', $course->start_dates)); ?></td>
+                            <td><?php echo implode(', ', explode(',', $course->durations)); ?></td>
+                            <td><?php echo implode(', ', explode(',', $course->prices)); ?></td>
                             <td>
                                 <a href="<?php echo admin_url('admin.php?page=dubkii-booking&delete_course=' . $course->id . '&active_tab=courses'); ?>" class="button">Delete</a>
                             </td>
@@ -212,6 +269,8 @@ function dubkii_booking_admin_page(){
                         <th>Course Name</th>
                         <th>Start Date</th>
                         <th>Duration</th>
+                        <th>Transport Cost</th>
+                        <th>Total</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -222,6 +281,18 @@ function dubkii_booking_admin_page(){
                             <td><?php echo $booking->course_name; ?></td>
                             <td><?php echo $booking->start_date; ?></td>
                             <td><?php echo $booking->duration; ?></td>
+                            <td>
+                                <?php 
+                                    $formatted_transport_cost = number_format($booking->transport_cost,2);
+                                    echo '$' . $formatted_transport_cost;
+                                ?>
+                            </td>
+                            <td>
+                                <?php 
+                                    $formatted_total_amount = number_format($booking->total_amount / 100, 2); 
+                                    echo '$' . $formatted_total_amount; // Display in currency format 
+                                ?>
+                            </td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
@@ -256,12 +327,27 @@ function dubkii_booking_admin_page(){
             });
         });
         function addDurationField() {
-            const wrapper = document.getElementById('duration-wrapper');
-            const input = document.createElement('input');
-            input.type = 'number';
-            input.name = 'durations[]';
-            input.placeholder = 'Duration in weeks';
-            wrapper.appendChild(input);
+            var durationWrapper = document.getElementById('duration-wrapper');
+            var newDurationField = document.createElement('div');
+            newDurationField.classList.add('duration-field');
+            
+            // Create duration input
+            var durationInput = document.createElement('input');
+            durationInput.type = 'number';
+            durationInput.name = 'durations[]';
+            durationInput.placeholder = 'Duration in weeks';
+            newDurationField.appendChild(durationInput);
+
+            // Create price input
+            var priceInput = document.createElement('input');
+            priceInput.type = 'number';
+            priceInput.name = 'prices[]';
+            priceInput.step = '0.01';
+            priceInput.placeholder = 'Price';
+            newDurationField.appendChild(priceInput);
+
+            // Add new fields to the wrapper
+            durationWrapper.appendChild(newDurationField);
         }
         function addStartDateField() {
             const wrapper = document.getElementById('start-date-wrapper');
